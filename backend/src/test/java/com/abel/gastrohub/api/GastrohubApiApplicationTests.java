86589842191c0610bfaee19bbc29acd425ea5828
Abel Moro <abel.moro@educa.madrid.org>
2025-04-12 22:07:48 +0200

    Inicialización del proyecto Spring Boot

---
+++ backend/src/test/java/com/abel/gastrohub/api/GastrohubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abel.gastrohub.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastrohubApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
