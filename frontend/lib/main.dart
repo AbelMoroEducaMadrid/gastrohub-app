c704ecdcd4decba32d15b280c6d1031393112110
Abel Moro <abel.moro@educa.madrid.org>
2025-05-15 21:58:21 +0200

    preparativos para llamadas a API REST

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gastrohub_app/src/screens/dashboard_screen.dart';
 import 'package:gastrohub_app/src/screens/login_screen.dart';
@@ -6,10 +7,19 @@ import 'package:gastrohub_app/src/core/themes/app_theme.dart';
 import 'package:gastrohub_app/src/screens/onboarding_screen.dart';
 import 'package:gastrohub_app/src/screens/registration_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await dotenv.load(fileName: '.env.local');
+    debugPrint(".env.local cargado");
+  } catch (_) {
+    await dotenv.load(fileName: '.env');
+    debugPrint(".env.local no encontrado, usando .env");
+  }
+
   runApp(const ProviderScope(child: MyApp()));
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
