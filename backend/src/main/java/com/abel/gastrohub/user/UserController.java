bd0164e72c8965e0913a870f51a2dcc75872054e
Abel Moro <abel.moro@educa.madrid.org>
2025-06-14 16:45:59 +0200

    Update UserController.java

--- backend/src/main/java/com/abel/gastrohub/user/UserController.java
+++ backend/src/main/java/com/abel/gastrohub/user/UserController.java
@@ -2,13 +2,13 @@ package com.abel.gastrohub.user;
 
 import com.abel.gastrohub.security.CustomUserDetails;
 import com.abel.gastrohub.user.dto.*;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import jakarta.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
